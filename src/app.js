e5edac8d8a6787dca0f16997c3ab2f51f733eca2
Thragull <146970246+Thragull@users.noreply.github.com>
2023-11-12 19:21:06 +0000

    excuses generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomNumber(length) {
+  return Math.floor(Math.random() * length);
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function excuse() {
+  let sentence = "";
+  sentence += who[randomNumber(who.length)];
+  sentence += " ";
+  sentence += action[randomNumber(action.length)];
+  sentence += " ";
+  sentence += what[randomNumber(what.length)];
+  sentence += " ";
+  sentence += when[randomNumber(when.length)];
+  sentence += ".";
+  return sentence;
+}
